ad8c996d51b6c95fbea4284a2bbb11c7169a02c1
JL-HEIG <jean-luc.blanc2@heig-vd.ch>
2021-09-28 14:50:10 +0200

    Finished credentials verification

--- Labo1 - kotlin - template/Labo1 - kotlin/app/src/main/java/ch/heigvd/iict/sym/labo1/MainActivity.kt
+++ Labo1 - kotlin - template/Labo1 - kotlin/app/src/main/java/ch/heigvd/iict/sym/labo1/MainActivity.kt
@@ -1,10 +1,15 @@
 package ch.heigvd.iict.sym.labo1
 
+import android.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.content.DialogInterface
+
+
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -52,7 +57,7 @@ class MainActivity : AppCompatActivity() {
             password.error = null
         }
 
-        validateButton.setOnClickListener {
+        validateButton.setOnClickListener { it ->
             //on réinitialise les messages d'erreur
             email.error = null
             password.error = null
@@ -76,6 +81,23 @@ class MainActivity : AppCompatActivity() {
                 return@setOnClickListener
             }
 
+            val credentialsInput = Pair(emailInput,passwordInput)
+
+            if(credentialsInput.equals(credentials[0]) || credentialsInput.equals(credentials[1])) {
+                //TODO lancer nouvelle activité
+            }
+            else {
+                val builder1: AlertDialog.Builder = AlertDialog.Builder(this)
+                builder1.setMessage("The credentials entered are not valid")
+                builder1.setCancelable(true)
+
+                builder1.setPositiveButton(
+                    "Ok",
+                    DialogInterface.OnClickListener { dialog, id -> dialog.cancel() })
+
+                val alert11: AlertDialog = builder1.create()
+                alert11.show()
+            }
             //TODO à compléter...
         }
     }
