6c2e848793f71935cf4619d773ce5d964ddbb439
Klab93 <kylian.bourcoud@gmail.com>
2021-10-05 18:46:12 +0200

    5.1 fait

--- Labo1 - kotlin - template/Labo1 - kotlin/app/src/main/java/ch/heigvd/iict/sym/labo1/Register.kt
+++ Labo1 - kotlin - template/Labo1 - kotlin/app/src/main/java/ch/heigvd/iict/sym/labo1/Register.kt
@@ -1,11 +1,27 @@
 package ch.heigvd.iict.sym.labo1
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
 
 class Register : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
     }
+
+    fun saveNewUser(view : View){
+        val userEmail = findViewById<EditText>(R.id.register_email).text?.toString();
+        val userPassword = findViewById<EditText>(R.id.register_password).text?.toString();
+        val intent = Intent(this, MainActivity::class.java).apply {
+            putExtra("newEmail", userEmail);
+            putExtra("newPassword", userPassword);
+        };
+        setResult(Activity.RESULT_OK, intent);
+        finish()
+    }
 }
+
