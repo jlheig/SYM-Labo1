10e3b33dccd1d790da4c45b36742707b823eb737
JL-HEIG <jean-luc.blanc2@heig-vd.ch>
2021-10-07 14:03:26 +0200

    Finished task 4

--- Labo1 - kotlin - template/Labo1 - kotlin/app/src/main/java/ch/heigvd/iict/sym/labo1/ProfileActivity.kt
+++ Labo1 - kotlin - template/Labo1 - kotlin/app/src/main/java/ch/heigvd/iict/sym/labo1/ProfileActivity.kt
@@ -8,20 +8,24 @@ import android.widget.ImageView
 import ch.heigvd.iict.sym.labo1.network.ImageDownloader
 
 class ProfileActivity : AppCompatActivity() {
+
+    private lateinit var tvEmail:TextView
+    private lateinit var ivProfilePic:ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+
+        tvEmail = findViewById(R.id.profile_tvEmail);
+        ivProfilePic = findViewById(R.id.profile_ivProfilePic);
 
         // Get the Intent that started this activity and extract the email
         val email = intent.getStringExtra(EXTRA_MESSAGE)
 
         // Capture the layout's TextView and set the email as its text
-        val textView = findViewById<TextView>(R.id.emailTxt).apply {
-            text = email
-        }
+        tvEmail.text = email
 
         //Profile picture
-        val profileImage = findViewById<ImageView>(R.id.profileImage)
-        ImageDownloader(profileImage, "https:thispersondoesnotexist.com/imaage").show()
+        ImageDownloader(ivProfilePic, "https:thispersondoesnotexist.com/image").show()
     }
 }
